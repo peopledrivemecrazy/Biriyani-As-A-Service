e595b7bcac4ae2949c70519bba8f442554a11634
Shriji <shrijike@gmail.com>
2020-07-05 19:32:37 +0530

    express-slow-down for rate limiting

--- biriyani.js
+++ biriyani.js
@@ -4,6 +4,8 @@ const polka = require('polka');
 const compress = require('compression')();
 const cors = require('cors');
 const expressGoogleAnalytics = require('express-google-analytics');
+const slowDown = require("express-slow-down");
+
 
 const analytics = expressGoogleAnalytics('UA-171692890-1');
 const baseURL = "https://biriyani.anoram.com/img/"
@@ -12,10 +14,17 @@ const assets = sirv('public', {
     maxAge: 31536000, // 1Y
     immutable: true
 });
+const ratelimit = slowDown({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    delayAfter: 5, // allow 5 requests to go at full-speed, then...
+    delayMs: 100 // 6th request has a 100ms delay, 7th has a 200ms delay, 8th gets 300ms, etc.
+  });
 polka()
+    .use("trust proxy")
     .use(cors())
     .use(analytics)
     .use(compress, assets)
+    .use(ratelimit)
     .get('/get', (req, res) => {
         let biriyanis = 21
         const randombiriyani = Math.floor(Math.random() * (biriyanis - 1) + 1);
